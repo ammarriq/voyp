5a0403f454a1eb511b6050165c6d17b5e67650c0
Ammar Iqbal <ammariqbal043@gmail.com>
2023-09-21 15:14:01 +0500

    fix: running pnpm run build fails

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { normalizePath } from 'vite'
+import path from 'path'
 
+// C:/project/foo
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-  ]
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: normalizePath(path.resolve(__dirname, './blog')),
+					dest: './'
+				}
+			]
+		})
+	]
 })
